a47f72a89adc9ed77459ebf3e8638a382aa2f2fe
sunnymodi29 <modisunny25@gmail.com>
2024-01-04 20:01:37 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-3xl font-bold m-4'>ECommerce</h1>
+      <div className='flex justify-center items-center m-4 gap-3'>
+        <img src='images/logo.png' alt='Logo' width={30}></img>
+        <h1 className='text-3xl font-bold'>WebDeals</h1>
+      </div>
       <h3 className='text-xl font-vold underline m-3 mt-0'><a className='cursor-pointer'>Our Products</a></h3>
     </div>
   );
