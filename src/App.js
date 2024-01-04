120e3e8e46f2d4236c658b8e77841f34c1dae917
sunnymodi29 <modisunny25@gmail.com>
2024-01-04 17:59:28 +0530

    setup tailwindcss

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>ECommerce</h1>
-      <h3>Our Products</h3>
+      <h1 className='text-3xl font-bold m-4'>ECommerce</h1>
+      <h3 className='text-xl font-vold underline m-3 mt-0'><a className='cursor-pointer'>Our Products</a></h3>
     </div>
   );
 }
